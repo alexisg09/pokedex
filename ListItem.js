c6d0188f251471adab5d2cb057018fe555ee7d2a
LRAV-Alexis <alexis.grenier@lrav.fr>
2022-10-14 12:40:12 +0200

    added enable on usequery

--- ListItem.js
+++ ListItem.js
@@ -1,19 +1,29 @@
-import { StyleSheet, Image, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+import { StyleSheet, Image, Text, View, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useQuery } from "@tanstack/react-query";
 import { fetchPokemons } from './api/fetchPokemons';
 import logo from './assets/favicon.png'
 
 export default function App() {
-  const { data, status, isLoading } = useQuery(["pokemons"], () => fetchPokemons());
+  const [enableFetch, setEnableFetch] = useState(false)
+  const { data, status, isLoading } = useQuery(["pokemons"], () => fetchPokemons(), { enabled: enableFetch });
   // console.log('data : ', data)
   console.log('status : ', status)
   console.log(data?.results)
 
   return (
-    <View styles={styles.container}>
+    <View style={styles.container}>
       <Text>Salut la team</Text>
-      {isLoading ? (<Text>Loading</Text>) : data.results !== undefined && data.results.map((poke) => {
+      <TouchableOpacity
+        onPress={() => setEnableFetch(true)}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Zé parti</Text>
+      </TouchableOpacity>
+      {status && (<Text>{status}</Text>)}
+
+      {status === 'success' && data.results.map((poke) => {
         <>
           <Image source={logo} style={{ width: 50, height: 50 }} />
           <Text>{poke.name}</Text>
@@ -31,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#DEDEDE',
     color: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
