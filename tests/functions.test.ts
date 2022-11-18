147be62cee961ba7edb227fa5b82703f65707c94
LRAV-Alexis <alexis.grenier@lrav.fr>
2022-11-18 14:03:19 +0100

    prettiered

--- tests/functions.test.ts
+++ tests/functions.test.ts
@@ -2,5 +2,8 @@ import { findColorFromType } from "../components/ItemDetails";
 
 test("Should return the correct colorTypeCombo given a specific type", () => {
   console.log(findColorFromType("Combat"));
-  expect(findColorFromType("Combat")).toStrictEqual({ type: 'Combat', color: 'coral' });
+  expect(findColorFromType("Combat")).toStrictEqual({
+    type: "Combat",
+    color: "coral",
+  });
 });
