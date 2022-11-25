e0be4d893571e33d3d734d6ffad0ab2ebefd5889
LRAV-Alexis <alexis.grenier@lrav.fr>
2022-11-25 09:16:13 +0100

    lint test

--- tests/functions.test.ts
+++ tests/functions.test.ts
@@ -1,9 +1,9 @@
-import { findColorFromType } from "../components/ItemDetails";
+import { findColorFromType } from '../components/ItemDetails';
 
-test("Should return the correct colorTypeCombo given a specific type", () => {
-  console.log(findColorFromType("Combat"));
-  expect(findColorFromType("Combat")).toStrictEqual({
-    type: "Combat",
-    color: "coral",
+test('Should return the correct colorTypeCombo given a specific type', () => {
+  console.log(findColorFromType('Combat'));
+  expect(findColorFromType('Combat')).toStrictEqual({
+    type: 'Combat',
+    color: 'coral',
   });
 });
